b8c975a6673bc766ca96763564fae434c6c85f5a
Zalihun69 <159771111+Zalihun69@users.noreply.github.com>
2026-02-27 10:16:39 +0100

    Add files via upload

--- kod.js
+++ kod.js
@@ -7,11 +7,11 @@ function visszaKep() {
 }
 
 function szamol(index) {
-    // Adott sor kiszámítása
+  
     let ar = document.getElementById("ar_" + index).innerText;
     let darab = document.getElementById("db_" + index).value;
     
-    // Ha üres vagy érvénytelen a bemenet, legyen 0
+ 
     if (darab == "" || darab < 0) {
         darab = 0;
     }
@@ -19,13 +19,13 @@ function szamol(index) {
     let reszosszeg = parseInt(ar) * parseInt(darab);
     document.getElementById("resz_" + index).innerText = reszosszeg;
 
-    // Teljes végösszeg újraszámolása (összesen 5 termék van: 0-tól 4-ig)
+  
     let osszesen = 0;
     for (let i = 0; i < 5; i++) {
         let aktualisResz = document.getElementById("resz_" + i).innerText;
         osszesen += parseInt(aktualisResz);
     }
 
-    // Kosár frissítése
+  
     document.getElementById("vegosszeg").innerText = osszesen;
 }
